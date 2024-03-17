237bc3fbd81889f394a22ba9b6ac52bfe4dc0c14
isa1989 <isaramazanov89@gmail.com>
2024-03-17 22:10:20 +0400

    f

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
@@ -17,16 +18,16 @@ from django.contrib.auth.password_validation import validate_password
 
 
 class RegisterBaseSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(
-        required=True, validators=[UniqueValidator(queryset=CustomerUser.objects.all())]
-    )
+    # email = serializers.EmailField(
+    #     required=True, validators=[UniqueValidator(queryset=CustomerUser.objects.all())]
+    # )
 
-    password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password]
-    )
+    # password = serializers.CharField(
+    #     write_only=True, required=True, validators=[validate_password]
+    # )
 
     class Meta:
-        model = User
+        model = CustomerUser
         fields = (
             # "username",
             "password",
@@ -46,7 +47,7 @@ class RegisterBaseSerializer(serializers.ModelSerializer):
 
     def save(self):
         """Serializer save, which pops unnecessary info from
-        `validated_data` and creates user object
+        validated_data and creates user object
 
         :return: user object (defined in Meta subclass) with given data
         :rtype: MyUser
@@ -54,41 +55,17 @@ class RegisterBaseSerializer(serializers.ModelSerializer):
 
         # Username = Email
         self.validated_data["username"] = self.validated_data["email"]
+        self.validated_data["password"] = make_password(
+            self.validated_data["password"]
+        )  # Hash the password
         user = self.Meta.model(**self.validated_data)
         user.is_active = False  # We did not eliminate is_active false for this reason ---https://stackoverflow.com/a/15122938/819982
         user.save()
         return user
 
-    # def validate(self, attrs):
-    #     if attrs["password"] != attrs["password2"]:
-    #         raise serializers.ValidationError(
-    #             {"password": "Password fields didn't match."}
-    #         )
-
-    #     return attrs
-
-    # def create(self, validated_data):
-    #     customer = CustomerUser.objects.create(
-    #         user_type="C",
-    #         username=validated_data["username"],
-    #         email=validated_data["email"],
-    #         first_name=validated_data["first_name"],
-    #         last_name=validated_data["last_name"],
-    #         fin_code=validated_data["fin_code"],
-    #         address=validated_data["address"],
-    #         phone_number=validated_data["phone_number"],
-    #         is_active=False,
-    #     )
-
-    #     customer.set_password(validated_data["password"])
-    #     customer.save()
-
-    #     return customer
-
 
 class CustomerRegisterSerializer(RegisterBaseSerializer):
-    class Meta:
-        model = CustomerUser
+    class Meta(RegisterBaseSerializer.Meta):
         fields = (
             "email",
             "first_name",
@@ -102,7 +79,7 @@ class CustomerRegisterSerializer(RegisterBaseSerializer):
 
 
 class CarRepairCompanyAgentRegisterSerializer(RegisterBaseSerializer):
-    class Meta:
+    class Meta(RegisterBaseSerializer.Meta):
         model = CarRepairCompanyAgent
         fields = (
             "email",
@@ -124,7 +101,7 @@ class CarRepairCompanyAgentRegisterSerializer(RegisterBaseSerializer):
 
 
 class LoginUserSerializer(serializers.Serializer):
-    username = serializers.CharField()
+    email = serializers.CharField()
     password = serializers.CharField()
 
     def validate(self, data):
